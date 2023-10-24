2722dab7afc28ce20ef1bad520f1c70e85b6ba64
zzzzbala <2051012034huy@ou.edu.vn>
2023-10-24 16:37:24 +0700

    update first lesson

--- app/dao.py
+++ app/dao.py
@@ -6,3 +6,41 @@ def load_categories():
         "id": 2,
         "name": "Tablet"
     }]
+
+
+def load_products():
+    return [{
+        "id": 1,
+        "name": "iPhone 7 Plus",
+        "description": "Apple, 32GB, RAM: 3GB, iOS13",
+        "price": 17000000,
+        "image": "https://e7.pngegg.com/pngimages/842/400/png-clipart-ipad-mini-2-ipad-air-ipad-1-ipad-2-ipad-electronics-gadget.png"
+    }, {
+        "id": 2,
+        "name": "iPad Pro 2020",
+        "description": "Apple, 128GB, RAM: 6GB",
+        "price": 37000000,
+        "image": "https://e7.pngegg.com/pngimages/842/400/png-clipart-ipad-mini-2-ipad-air-ipad-1-ipad-2-ipad-electronics-gadget.png"
+
+    }, {
+        "id": 3,
+        "name": "Galaxy Note 10 Plus",
+        "description": "Samsung, 64GB, RAML: 6GB",
+        "price": 24000000,
+        "image": "https://e7.pngegg.com/pngimages/842/400/png-clipart-ipad-mini-2-ipad-air-ipad-1-ipad-2-ipad-electronics-gadget.png"
+
+    }, {
+        "id": 4,
+        "name": "Galaxy Note 10 Plus",
+        "description": "Samsung, 64GB, RAML: 6GB",
+        "price": 24000000,
+        "image": "https://e7.pngegg.com/pngimages/842/400/png-clipart-ipad-mini-2-ipad-air-ipad-1-ipad-2-ipad-electronics-gadget.png"
+
+    }, {
+        "id": 5,
+        "name": "Galaxy Note 10 Plus",
+        "description": "Samsung, 64GB, RAML: 6GB",
+        "price": 24000000,
+        "image": "https://e7.pngegg.com/pngimages/842/400/png-clipart-ipad-mini-2-ipad-air-ipad-1-ipad-2-ipad-electronics-gadget.png"
+
+    }]
--- app/index.py
+++ app/index.py
@@ -1,12 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import dao
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    kw = request.args.get("kw")
     cate = dao.load_categories()
-    return render_template("index.html", categories=cate)
+    products = dao.load_products()
+
+    if kw:
+        products = [p for p in products if p['name'].find(kw) >= 0]
+
+    return render_template("index.html", categories=cate, products=products)
 
 if __name__ == "__main__":
     app.run(debug=True)
